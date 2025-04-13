c18644023d5a7c508817f36df00210ddb18d60a2
Ankur <singhankur362@gmail.com>
2025-04-14 02:55:25 +0530

    Completed Code Editor Page

--- src/components/EndCallButton.tsx
+++ src/components/EndCallButton.tsx
@@ -3,6 +3,7 @@ import { useMutation, useQuery } from "convex/react";
 import { useRouter } from "next/navigation";
 import { api } from "../../convex/_generated/api";
 import { Button } from "./ui/button";
+import toast from "react-hot-toast";
 
 const EndCallButton = () => {
   const call = useCall();
@@ -10,7 +11,7 @@ const EndCallButton = () => {
   const { useLocalParticipant } = useCallStateHooks();
   const localParticipant = useLocalParticipant();
 
-  const updateInterviewsStatus = useMutation(
+  const updateInterviewStatus = useMutation(
     api.interviews.updateInterviewStatus
   );
 
@@ -18,14 +19,33 @@ const EndCallButton = () => {
     streamCallId: call?.id || "",
   });
 
-  if(!call || !interview) return null;
+  if (!call || !interview) return null;
 
-  const isMeetingOwner = localParticipant?.userId === call.state.createdBy?.id
+  const isMeetingOwner = localParticipant?.userId === call.state.createdBy?.id;
 
-  if(!isMeetingOwner) return null;
+  if (!isMeetingOwner) return null;
 
-  return <Button>
-    End Meeting
-  </Button>
+  const endCall = async () => {
+    try {
+      await call.endCall();
+
+      await updateInterviewStatus({
+        id: interview._id,
+        status: "completed",
+      });
+
+      router.push("/");
+      toast.success("Meeting ended for everyone");
+    } catch (error) {
+      console.log(error);
+      toast.error("Failed to end meeting");
+    }
+  };
+
+  return (
+    <Button variant={"destructive"} onClick={endCall}>
+      End Meeting
+    </Button>
+  );
 };
 export default EndCallButton;
