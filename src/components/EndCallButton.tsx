471366ab752b13dc533696f547b035d99e4a825b
Ankur <singhankur362@gmail.com>
2025-04-14 00:28:56 +0530

    Completed Meeting Room page

---
+++ src/components/EndCallButton.tsx
@@ -0,0 +1,31 @@
+import { useCall, useCallStateHooks } from "@stream-io/video-react-sdk";
+import { useMutation, useQuery } from "convex/react";
+import { useRouter } from "next/navigation";
+import { api } from "../../convex/_generated/api";
+import { Button } from "./ui/button";
+
+const EndCallButton = () => {
+  const call = useCall();
+  const router = useRouter();
+  const { useLocalParticipant } = useCallStateHooks();
+  const localParticipant = useLocalParticipant();
+
+  const updateInterviewsStatus = useMutation(
+    api.interviews.updateInterviewStatus
+  );
+
+  const interview = useQuery(api.interviews.getInterviewByStreamCallId, {
+    streamCallId: call?.id || "",
+  });
+
+  if(!call || !interview) return null;
+
+  const isMeetingOwner = localParticipant?.userId === call.state.createdBy?.id
+
+  if(!isMeetingOwner) return null;
+
+  return <Button>
+    End Meeting
+  </Button>
+};
+export default EndCallButton;
